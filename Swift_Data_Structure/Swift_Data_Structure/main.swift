7d2f3b6832297c93ca3d1d56c3914e6630ecd82e
jake5113 <jongwon5113@gmail.com>
2023-10-04 17:44:31 +0900

    Data Structure

--- Swift_Data_Structure/Swift_Data_Structure/main.swift
+++ Swift_Data_Structure/Swift_Data_Structure/main.swift
@@ -7,7 +7,65 @@
 
 import Foundation
 
-print("Hello, World!")
+func brackets() {
+    var stack : [String] = []
+    var result : [String] = []
+    let bracketsList : [String] = ["(", ")", "{", "}", "[", "]"]
+    
+    var inputBracket = ""
+    
+    while inputBracket != "q" {
+        inputBracket = readLine()!
+        if bracketsList.contains(inputBracket) {
+            
+            result.append(inputBracket)
+            
+            switch inputBracket {
+            case ")":
+                isPopable("(")
+            case "}" :
+                isPopable("{")
+            case "]" :
+                isPopable("[")
+            default:
+                stack.append(inputBracket)
+            }
+        }
+    }
+    
+    print(result.joined())
+    print(stack, stack.isEmpty)
+    
+    func isPopable(_ bracket:String) {
+        if(stack.last == bracket) {
+            stack.removeLast()
+        } else {
+            stack.append(inputBracket)
+        }
+    }
+}
 
+func postfix() {
+//    let basicOperations : [String] = ["+", "-", "*", "/"]
+//    
+//    let intArr = readLine()!.split(separator: " ").map { $0 }
+//    var inputOperations = ""
+//    
+//    if basicOperations.contains(inputOperations) {
+//        switch basicOperations {
+//        case "+":
+//            
+//        case "-" :
+//            
+//        case "*" :
+//            
+//        case "/" :
+//            
+//        default:
+//            stack.append(inputBracket)
+//        }
+//        print(intArr)
+//    }
+}
 
-
+postfix()
